1e178e8ae0124b0a32e0473a63e2efe856db1fbe
cam.lafit <cam.lafit@azerttyu.net>
2014-03-28 10:50:44 +0100

    Check if is it a Squeeze
    * We can update only from squeeze

--- alternc_update_squeeze_3.sh
+++ alternc_update_squeeze_3.sh
@@ -2,7 +2,22 @@
 
 echo "Alternc update to 3"
 
+#Initialization variables
+IS_SQUEEZE=false
+
 #Check squeeze status
+if [ -f /etc/debian_version ]; then
+    DEBIAN_VERSION="$(</etc/debian_version)"
+    DEBIAN_VERSION_MAJOR=${DEBIAN_VERSION:0:1}
+    if [ $DEBIAN_VERSION_MAJOR == 6 ]; then
+        IS_SQUEEZE=true
+    fi
+fi
+
+if [[ ${IS_SQUEEZE} == false ]]; then
+    echo "Squeeze is required"
+    exit;
+fi
 
 #Check version alternc
 
@@ -12,8 +27,10 @@ echo "Alternc update to 3"
 
 #Update source.list
 
-#Update Alternc
+#Provide a cert to Dovecot
 
-#Restore OpenDKIM
+#Kill Courier
 
-#Wash old packages
+#Disable Courier
+
+
