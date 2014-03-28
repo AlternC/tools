c98c0a3800de7209f8a56d47b830e35bca3099e0
cam.lafit <cam.lafit@azerttyu.net>
2014-03-28 10:42:20 +0100

    Provide a script to update Alternc 1.x to 3.x in Squeeze
    * Some problems are presents in squeeze, then alternc update is not trivial

---
+++ alternc_update_squeeze_3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+echo "Alternc update to 3"
+
+#Check squeeze status
+
+#Check version alternc
+
+#Check alternc Directory
+
+#Check ACL
+
+#Update source.list
+
+#Update Alternc
+
+#Restore OpenDKIM
+
+#Wash old packages
