ebff0194d36a0973a6b45490aee36043de0a0020
Your Name <you@example.com>
2015-10-21 23:33:29 +0200

    [enh] first working version of feature-migrate-domains

--- migrate_domains/lib/Alternc/Tools/Domains/Import.php
+++ migrate_domains/lib/Alternc/Tools/Domains/Import.php
@@ -16,15 +16,15 @@ class Alternc_Tools_Domains_Import {
     /**
      *
      * Class from alternc project
-     * @var m_domain
+     * @var m_dom
      */
     protected $domain;
 
     /**
      *
-     * @var int
+     * @var array 
      */
-    public $default_quota = 1024;
+    public $domainesTypeCache = array();
 
     /**
      *
@@ -38,12 +38,14 @@ class Alternc_Tools_Domains_Import {
      * @throws \Exception
      */
     function __construct($options) {
+	global $dom;
         // Attempts to retrieve db
         if (isset($options["db"])) {
             $this->db = $options["db"];
         } else {
             throw new \Exception("Missing parameter db");
         }
+	$this->domain = $dom; 
     }
 
     /**
@@ -77,27 +79,40 @@ class Alternc_Tools_Domains_Import {
      */
     function checkLoginExists($domainData) {
 
+	global $cuid;
+	global $get_quota_cache;
         $field = $domainData["login"];
         $success = false;
-
+	
         // Build query
-        $query = "SELECT login "
+        $query = "SELECT uid "
                 . "FROM membres u "
                 . "WHERE u.login = '" . addslashes($field) . "'";
 
+
         // No record ? Exit
-        if (count($this->query($query))) {
+        $result = $this->query($query);
+	if (count( $result )) {
+	    $cuid = $result[0]["uid"];
+	    $this->giveQuota( array("cuid" => $cuid, "type" => "dom") );
             $success = true;
         }
-        return $success;
+	return $success;
+    }
+
+    function giveQuota( $options ){
+
+	    global $cuid;
+	    global $get_quota_cache;
+	    $cuid = $options["cuid"];
+	    $type = $options["type"];
+	    $get_quota_cache[$cuid][$type] = array( "u" => 0, "t" => 1);
+
     }
 
 
     /**
      * Creates the domain
-     * 
-     * Used the domain_add.php script by Antoine Beaupré for details. 
-     * Snarky comments tend to be the most useful ones around here.
      * 
      * @global type $err
      * @param type $domainData
@@ -108,16 +123,73 @@ class Alternc_Tools_Domains_Import {
     function createDomain($domainData, $commandLineResult) {
 
         global $err;
+	global $cuid;
 
         // Retrieves command line options 
         $options = $commandLineResult->options;
         
+	// Retrieve forced uid if exists
+        $force_uid = (int) $options["force_uid"];
+  
+	if( $force_uid ) {
+	    $this->giveQuota( array("cuid" => $force_uid, "type" => "dom" ) );
+	} 
+	 
         // Retrieve domain
         $domain = $domainData["domaine"];
-        
-        // Retrieve forced uid if exists
-        
-        
+        $dns = $domainData["gesdns"];
+        $dns_action = $domainData["dns_action"];
+        $dns_result = $domainData["dns_result"];
+        $mail = $domainData["gesmx"];
+        $zonettl = $domainData["zonettl"];
+        $noerase = $domainData['noerase'];
+ 	$force = true;
+	$isslave = false;
+	$slavedom = "";
+	// Attempts to insert domain
+	$this->domain->lock();
+       	if( ! $this->domain->add_domain( $domain, $dns, $noerase, $force, $isslave, $slavedom) ){
+	    throw new Exception("Failed to create $domain : ".$err->errstr() );	
+	}
+
+	// Retrieve new domain id
+	$query = "SELECT id FROM domaines WHERE domaine='$domain'";
+	$result = current( $this->query( $query ) );
+	$domain_id = (int) $result['id'];
+	if( ! $domain_id ){
+	    throw new Exception("Failed to retrieve domain_id for $domain " );	
+	}
+
+	// Attempts to insert subdomains
+
+	$subdomainsList = $domainData["sub_domains"];
+	$subdomainsErrorList = array();
+	foreach( $subdomainsList as $subdomainData ){
+
+		$compte = $subdomainData["compte"];
+		$domain = $subdomainData["domaine"];
+		$sub = $subdomainData["sub"];
+		$dest = $subdomainData["valeur"];
+		$type = $subdomainData["type"];
+		$web_action = $subdomainData["web_action"];
+		$web_result = $subdomainData["web_result"];
+		$enable = $subdomainData["enable"];
+		if( ! array_key_exists( $type, $this->domainesTypeCache ) ){
+			$valid = $this->domain->domains_type_target_values( $type );
+			$this->domainesTypeCache[$type] = $valid;
+		}
+		if( ! $this->domainesTypeCache[$type] ){
+			$subdomainsErrorList[] = "Type '$type' is invalid ($sub.$domain)";
+			continue;
+		}
+
+		if( ! $this->domain->set_sub_domain($domain, $sub, $type, $dest ) ) {
+			$subdomainsErrorList[] = "Failed to create $sub.$domain : ". $err->errstr();	
+		}
+	}
+	if( count( $subdomainsErrorList ) ){
+		return array("code" => 1, "message" => "Errors with subdomains : ".implode( " / ",$subdomainsErrorList) );
+	}
 
         return array("code" => 0, "message" => "ok", "domain_id" => $domain_id);
     }
@@ -143,7 +215,7 @@ class Alternc_Tools_Domains_Import {
         // Decode from JSON
         $exportList = json_decode($file_content, true);
         if (json_last_error()) {
-            throw new Exception("JSON encoding failed: " . json_last_error_msg());
+            throw new Exception("JSON encoding failed: " . json_last_error());
         }
 
         // Exit
@@ -174,25 +246,25 @@ class Alternc_Tools_Domains_Import {
         // Loop through domains
         foreach ($exportList as $domainData) {
 
-            $domain = $domainData["domain"];
+            $domain = $domainData["domaine"];
             try {
 
                 // Login not exists : error
-                if (!$this->checkLoginExists($domainData)  && ! is_null($force_uid) ) {
+                if (!$this->checkLoginExists($domainData) &&  ! $force_uid ) {
                     throw new Exception("Login does not exist: " . $domainData["login"] . " for $domain ");
                 }
 
                 // Domain not exists : error 
-                if (!$this->checkDomainExists($domainData)) {
-                    throw new Exception("Domain does not exist: " . $domainData["domain"] . " for $domain ");
+                if ($this->checkDomainExists($domainData)) {
+                    throw new Exception("Domain exists: " . $domain );
                 }
 
                 // Create domain
-                $creationResult = $this->createDomain($domainData, $options );
+                $creationResult = $this->createDomain($domainData, $commandLineResult);
 
                 // Failed to create? 
                 if (!isset($creationResult["code"]) || $creationResult["code"] != 0) {
-                    throw new Exception("Edomain creation error: " . $creationResult["message"] . " for $domain ");
+                    throw new Exception("Domain $domain creation error: " . $creationResult["message"] );
                 }
 
                 // Attempts to retrieve domain_id
@@ -202,9 +274,6 @@ class Alternc_Tools_Domains_Import {
                     throw new Exception("Missing parameter domain_id");
                 }
 
-                // Add to export for rsync
-                $rsyncExport[] = $this->buildRsyncExportSet($domain_id, $domainData);
-
                 // Record success
                 $successList[] = $domain;
 
@@ -213,8 +282,6 @@ class Alternc_Tools_Domains_Import {
                 $errorsList[] = $e->getMessage();
             }
         }
-
-        $this->writeRsyncExport($rsyncExport, $commandLineResult);
 
         $return["successList"] = $successList;
 
@@ -252,113 +319,6 @@ class Alternc_Tools_Domains_Import {
         return $recordList;
     }
 
-    /**
-     * 
-     * @param Console_CommandLine_Result $commandLineResult
-     * @return boolean
-     * @throws Exception
-     */
-    function rsync($commandLineResult) {
 
-        // Attempts to read export
-        $exportList = $this->getRsyncExport($commandLineResult);
-
-        // Retrieves options
-        $options = $commandLineResult->options;
-        
-        // Attempts to retrieve log_file
-        if (isset($options["log_file"]) && $options["log_file"]) {
-            $log_file = $options["log_file"];
-        } else {
-            throw new \Exception("Missing parameter log_file");
-        }
-        
-        // Retrieve arguments
-        $argumentsList = $commandLineResult->args;
-
-        // Attempts to retrieve source
-        if (isset($argumentsList["source"]) && $argumentsList["source"]) {
-            $source = $argumentsList["source"];
-        } else {
-            throw new \Exception("Missing parameter source");
-        }
-
-        // Attempts to retrieve target
-        if (isset($options["target"]) && $argumentsList["target"]) {
-            $target = $options["target"];
-        } else {
-            throw new \Exception("Missing parameter target");
-        }
-
-        // Loop through [new,old] sets
-        foreach ($exportData as $set) {
-
-            $errorList = array();
-
-            try {
-
-                // Attempts to retrieve domain
-                if (isset($set["domain"]) && $set["domain"]) {
-                    $domain = $set["domain"];
-                } else {
-                    throw new \Exception("Missing parameter domain for $domain");
-                }
-                // Attempts to retrieve new_path
-                if (isset($set["new_path"]) && $set["new_path"]) {
-                    $new_path = $set["new_path"];
-                } else {
-                    throw new Exception("Missing parameter new_path for $domain");
-                }
-                // Attempts to retrieve old_path
-                if (isset($set["old_path"]) && $set["old_path"]) {
-                    $old_path = $set["old_path"];
-                } else {
-                    throw new \Exception("Missing parameter old_path");
-                }
-
-                // Build the RSYNC command, note we don't keep owner and group
-                $command = "rsync -rlpt " . escapeshellarg("$source:$old_path") . " " . escapeshellarg("$target:$new_path >> ".  escapeshellarg($log_file));
-                
-                // Run the Rsync command
-                $code = 1;
-                $output = array();
-                exec( $command, $output, $code);
-                if( $code != 0 ){
-                    throw new Exception("Rsync failed for $domain : code#${code}, output : ".  json_encode($output));
-                }
-                
-            } catch (Exception $exc) {
-                $errorList[] = $exc->getTraceAsString();
-            }
-        }
-    }
-
-    /**
-     * Writes a JSON file ready for use by the RSYNC function
-     * 
-     * @param array $exportList
-     * @param Console_CommandLine_Result $commandLineResult
-     * @return boolean
-     * @throws Exception
-     */
-    function writeRsyncExport($exportList, $commandLineResult) {
-
-        // Retrieves command line options 
-        $options = $commandLineResult->options;
-
-        // Encode to JSON
-        $export_content = json_encode($exportList);
-        if (json_last_error()) {
-            throw new Exception("JSON encoding failed: " . json_last_error_msg());
-        }
-
-        // Write to output
-        $output_file = $options["output_file"] ? $options["output_file"] : $this->default_output;
-        if (!file_put_contents($output_file, $export_content)) {
-            throw new Exception("Failed to write export $output_file");
-        }
-
-        return true;
-    }
 
 }
